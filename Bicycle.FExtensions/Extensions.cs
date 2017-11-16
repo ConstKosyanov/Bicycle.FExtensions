6dacc8e728a9e70276403ba621824fd5a9cdf7ce
Константин Косьянов <constkosyanov@outlook.com>
2017-11-16 17:12:45 +0300

    Added overloaded method "Do", which can return different types

--- Bicycle.FExtensions/Extensions.cs
+++ Bicycle.FExtensions/Extensions.cs
@@ -6,13 +6,12 @@ namespace Bicycle.FExtensions
     public static class Extensions
     {
         public static bool In<T>(this T local, params T[] args) => args.Contains(local);
-
         public static T Do<T>(this T local, Action<T> action)
         {
             action(local);
             return local;
         }
-
+        public static TOut Do<TIn, TOut>(this TIn local, Func<TIn, TOut> func) => func(local);
         public static T To<T>(this object local) => (T)local;
         public static T As<T>(this object local) where T : class => local as T;
     }
