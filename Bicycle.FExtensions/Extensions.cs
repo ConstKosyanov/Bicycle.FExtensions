f75265ec97e877d3269f95d4349a914171d89696
ConstKosyanov <constkosyanov@gmail.com>
2017-12-19 15:36:30 +0300

    Added overloaded method "Do" that takes function as parameter but returns the same original object after function call, not the funcion result.

--- Bicycle.FExtensions/Extensions.cs
+++ Bicycle.FExtensions/Extensions.cs
@@ -11,6 +11,11 @@ namespace Bicycle.FExtensions
             action(local);
             return local;
         }
+        public static T Do<T,Tout>(this T local, Func<T,Tout> func)
+        {
+            func(local);
+            return local;
+        }
         public static TOut Make<TIn, TOut>(this TIn local, Func<TIn, TOut> func) => func(local);
         public static T To<T>(this object local) => (T)local;
         public static T As<T>(this object local) where T : class => local as T;
